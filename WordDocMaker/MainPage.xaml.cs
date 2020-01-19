2143bf5b544754f0e6c56ad6642751be3e5a92f7
ArihantBapna <greatbapna@gmail.com>
2020-01-18 19:49:52 -0500

    ProjectCreate

---
+++ WordDocMaker/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Xamarin.Forms;
+
+namespace WordDocMaker
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            //"App" is the class of Portable project
+            Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+
+            //Creating a new document
+            WordDocument document = new WordDocument();
+            //Adding a new section to the document
+            WSection section = document.AddSection() as WSection;
+            //Set Margin of the section
+            section.PageSetup.Margins.All = 72;
+            //Set page size of the section
+            section.PageSetup.PageSize = new Syncfusion.Drawing.SizeF(612, 792);
+
+            //Create Paragraph styles
+            WParagraphStyle style = document.AddParagraphStyle("Normal") as WParagraphStyle;
+            style.CharacterFormat.FontName = "Calibri";
+            style.CharacterFormat.FontSize = 11f;
+            style.ParagraphFormat.BeforeSpacing = 0;
+            style.ParagraphFormat.AfterSpacing = 8;
+            style.ParagraphFormat.LineSpacing = 13.8f;
+
+            style = document.AddParagraphStyle("Heading 1") as WParagraphStyle;
+            style.ApplyBaseStyle("Normal");
+            style.CharacterFormat.FontName = "Calibri Light";
+            style.CharacterFormat.FontSize = 16f;
+            style.CharacterFormat.TextColor = Syncfusion.Drawing.Color.FromArgb(46, 116, 181);
+            style.ParagraphFormat.BeforeSpacing = 12;
+            style.ParagraphFormat.AfterSpacing = 0;
+            style.ParagraphFormat.Keep = true;
+            style.ParagraphFormat.KeepFollow = true;
+            style.ParagraphFormat.OutlineLevel = OutlineLevel.Level1;
+            IWParagraph paragraph = section.HeadersFooters.Header.AddParagraph();
+
+            paragraph.ApplyStyle("Normal");
+            paragraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Left;
+            WTextRange textRange = paragraph.AppendText("Adventure Works Cycles") as WTextRange;
+            textRange.CharacterFormat.FontSize = 12f;
+            textRange.CharacterFormat.FontName = "Calibri";
+            textRange.CharacterFormat.TextColor = Syncfusion.Drawing.Color.Red;
+
+            //Appends paragraph
+            paragraph = section.AddParagraph();
+            paragraph.ApplyStyle("Heading 1");
+            paragraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Center;
+            textRange = paragraph.AppendText("Adventure Works Cycles") as WTextRange;
+            textRange.CharacterFormat.FontSize = 18f;
+            textRange.CharacterFormat.FontName = "Calibri";
+           
+            //Appends paragraph
+            section.AddParagraph();
+
+            //Saves the Word document to MemoryStream
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream, FormatType.Docx);
+
+            //Save the stream as a file in the device and invoke it for viewing
+            await Xamarin.Forms.DependencyService.Get<ISave>().SaveAndView("Sample.docx", "application/msword", stream);
+        }
+    }
+}
